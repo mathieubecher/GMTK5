b16d64fb20c7154623a2862ae0cef92ec8b2139c
Becher <mbelkhenchir@gmail.com>
2023-07-08 21:02:46 +0200

    Add animator on haykart, and light in scene (horrible)

--- Assets/Scripts/Character/Haykart.cs
+++ Assets/Scripts/Character/Haykart.cs
@@ -6,11 +6,13 @@ public class Haykart : MonoBehaviour
 {
     [SerializeField] private float m_speed = 5.0f;
 
+    private Animator m_animator;
     private Rigidbody m_rigidbody;
 
     private void Awake()
     {
         m_rigidbody = GetComponent<Rigidbody>();
+        m_animator = GetComponent<Animator>();
     }
         
     private void Update()
@@ -26,17 +28,29 @@ public class Haykart : MonoBehaviour
 
     private void ReadMovement()
     {
-        m_rigidbody.velocity = Controller.instance.moveInput * m_speed;
+        Vector3 moveInput = Controller.instance.moveInput;
+        m_rigidbody.velocity = moveInput * m_speed;
+        m_animator.SetFloat("x", moveInput.x);
+        m_animator.SetFloat("y", moveInput.z);
     }
     
     private void OnCollisionEnter(Collision _other)
     {
-        ResetMovement();
-        m_rigidbody.isKinematic = true;
         Vector3 position = transform.position;
         position.y = _other.transform.parent.position.y + 0.1f;
         transform.position = position;
         
+        Loose();
+    }
+
+    private void Loose()
+    {
+        ResetMovement();
+        m_rigidbody.isKinematic = true;
         GameManager.instance.PauseGame();
+        
+        m_animator.SetFloat("x", 0.0f);
+        m_animator.SetFloat("y", 0.0f);
+        m_animator.SetBool("dead", true);
     }
 }
