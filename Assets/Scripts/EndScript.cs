8dd2ad45d2257d26d44a3135ffd7ad29c16a5aae
Becher <mbelkhenchir@gmail.com>
2023-07-09 03:48:58 +0200

    Random pos for assassin

--- Assets/Scripts/EndScript.cs
+++ Assets/Scripts/EndScript.cs
@@ -1,8 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EndScript : MonoBehaviour
 {
     [SerializeField] private Transform m_assassinPos;
+    [SerializeField] private Vector2 m_distance;
+    
+    private void Awake()
+    {
+        GameManager.OnGameStart += GameStart;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.OnGameStart -= GameStart;
+    }
+
+    private void GameStart()
+    {
+        Vector3 position = m_assassinPos.position;
+        position.x = Random.Range(- m_distance.x - 2.0f, m_distance.x + 2.0f);
+        position.y = Random.Range(- m_distance.y - 2.0f, m_distance.y + 2.0f);
+        m_assassinPos.position = position;
+    }
 }
